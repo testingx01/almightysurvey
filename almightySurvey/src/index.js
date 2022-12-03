915f1137b267a8854d30d93d09ad9c7c760bb4af
Juned Ansari <67496094+testingx01@users.noreply.github.com>
2022-12-04 03:36:12 +0530

    Update index.js

--- almightySurvey/src/index.js
+++ almightySurvey/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createRoot } from 'react-dom/client';
-import SurveyCreatorComponent from './SurveyCreatorComponent';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-const createSurvey = createRoot(document.getElementById("surveyCreatorContainer"));
-createSurvey.render(<SurveyCreatorComponent />);
